bf5baa94e42dbe82fa5b61240a3a5c5e2f732d15
hesam <hesam.naderi.kotlin@gmail.com>
2023-12-23 14:20:55 +0330

    add: entity to Note data class

--- app/src/main/java/com/model/Note.kt
+++ app/src/main/java/com/model/Note.kt
@@ -2,12 +2,22 @@ package com.model
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
 import java.time.LocalDateTime
+import java.util.Date
 import java.util.UUID
 
+@Entity(tableName = "notes_tbl")
 data class Note @RequiresApi(Build.VERSION_CODES.O) constructor(
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
+    @ColumnInfo(name = "note_title")
     val title: String,
+    @ColumnInfo(name = "note_description")
     val description: String,
+    @ColumnInfo(name = "note_entry_date")
     val entryDate: LocalDateTime = LocalDateTime.now()
 )

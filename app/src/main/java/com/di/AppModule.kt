64a7cb1ef9419903549e5b59c678041daf5bb1c4
hesam <hesam.naderi.kotlin@gmail.com>
2023-12-25 22:39:45 +0330

    add: dao and app module

--- app/src/main/java/com/di/AppModule.kt
+++ app/src/main/java/com/di/AppModule.kt
@@ -1,10 +1,26 @@
 package com.di
 
+import android.content.Context
+import androidx.room.Room
+import com.data.NoteDatabase
+import com.data.NoteDatabaseDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+    @Singleton
+    @Provides
+    fun provideNotesDao(noteDatabase: NoteDatabase): NoteDatabaseDao = noteDatabase.notDao()
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): NoteDatabase =
+        Room.databaseBuilder(context, NoteDatabase::class.java, "note_database.db")
+            .fallbackToDestructiveMigration().build()
 }
